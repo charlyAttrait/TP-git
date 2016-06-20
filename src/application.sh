a1728fc3431b6c0978d5adaaa933cf3c314f6a42
Charly Attrait <c.attrait@iia-laval.fr>
2016-06-20 16:09:48 +0200

    add var to script

--- src/application.sh
+++ src/application.sh
@@ -1,4 +1,7 @@
 ### application.sh - 2016 - Charly ATTRAIT ###
-echo "Build : 10:00"
+version = "2"
+build_hour = "11H00"
+
+echo "Version $[version]"
+echo "Build : $[build_hour]"
 echo "Copyright company - 2016"
-echo "Version 1.1"
